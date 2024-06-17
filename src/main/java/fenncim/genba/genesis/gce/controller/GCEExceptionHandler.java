16553d7a7e3fbb60c453da3d0bdea23391aceff5
fenn <fabrice.edgar@gmail.com>
2024-06-17 10:09:32 +0200

    clean code

--- src/main/java/fenncim/genba/genesis/gce/controller/GCEExceptionHandler.java
+++ src/main/java/fenncim/genba/genesis/gce/controller/GCEExceptionHandler.java
@@ -8,7 +8,6 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.HttpMediaTypeNotAcceptableException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.bind.annotation.ResponseStatus;
 
 @ControllerAdvice
 public class GCEExceptionHandler {
